8340dd587189388dd7356551cb4b4acb4b272bbb
mrKruq <lukaszkruk32@onet.pl>
2024-09-18 15:54:08 +0200

    Wlasnosc statycza

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,18 @@ public class Main {
         System.out.println(osobaX.getWiek());
 
         System.out.println(osobaIsia);
-
+        System.out.println("Liczba utworzonych obiektów: "+Uczen.liczbaObiektow);
         Uczen uczenEmil = new Uczen("Emil", "Smaladzki", 7, 123);
         System.out.println(uczenEmil);
+        System.out.println("Liczba utworzonych obiektów: "+Uczen.liczbaObiektow);
+        Uczen uczen2 =new Uczen("Szymon","Grajek",11);
+        System.out.println("Liczba utworzonych obiektów: "+Uczen.liczbaObiektow);
+        Uczen uczen3 =new Uczen("Asia","Menda",12);
+        System.out.println("Liczba utworzonych obiektów: "+Uczen.liczbaObiektow);
+        Uczen uczen4 =new Uczen("Lucjan","Wałek",13);
+        System.out.println(uczen4);
+        System.out.println("Liczba utworzonych obiektów: "+Uczen.liczbaObiektow);
+
 
     }
 }
--- src/Uczen.java
+++ src/Uczen.java
@@ -5,20 +5,25 @@ public class Uczen extends Osoba{
     Uczen dziedziczy wszystko? po klasie Osoba
     */
     private int nrEwidencyjny;
+    static int liczbaObiektow;
 
     public Uczen(String imie, String nazwisko, int wiek) {
         super(imie, nazwisko, wiek);//wywolujemy konstruktor klasy bazowej
+        liczbaObiektow++;
+        nrEwidencyjny =liczbaObiektow;
     }
 
     public Uczen(String imie, String nazwisko, int wiek, int nrEwidencyjny) {
         super(imie, nazwisko, wiek);
         this.nrEwidencyjny = nrEwidencyjny;
+        liczbaObiektow++;
+
     }
 
     @Override
     public String toString() {
         return "Uczen imie:"+getImie()+" nazwisko: "+nazwisko +
-                "nrEwidencyjny=" + nrEwidencyjny +
-                '}';
+                " nrEwidencyjny=" + nrEwidencyjny +
+                ' ';
     }
 }
